047819f338a19e7a4d98c090726eb46091a53014
Kevin Lacker <lacker@gmail.com>
2016-02-06 16:35:41 -0800

    mcuk around

---
+++ src/EmojiSelector.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+ 
+
+const emoji = require('emoji-named-characters');
+
+let allEmoji = [];
+for (let e in emoji) {
+  allEmoji.push(e);
+}
+
+function randomEmojiName() {
+  return allEmoji[Math.floor(Math.random() * allEmoji.length)];
+}
+
+function randomEmoji() {
+  return emoji[randomEmojiName()].character;
+}
+
+class EmojiSelector extends React.Component {
+  render() {
+    let emojiList = 
+    return emojiList.map(emoji => {
+      return (
+        <div>
+          {emoji}
+        </div>
+      );
+    });
+  }
+
+});
+
+ReactDOM.render(<EmojiSelector/>, document.getElementById('hello'));
